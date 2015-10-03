867856c724468633c0ca1cdfec136e010ca08a90
Kurt Smith <kwmsmith@gmail.com>
2015-10-03 15:19:18 -0500

    More functionality and tests.

--- test_vector.py
+++ test_vector.py
@@ -1,28 +1,44 @@
-from vector import Vector
+from vector import vec
 from random import shuffle
 
 def test_creation_empty():
-    v = Vector()
+    v = vec()
     assert len(v) == 0
-    w = Vector([])
+    w = vec([])
     assert len(w) == 0
 
 
 def test_creation_non_empty():
-    v = Vector([1, 2, 3])
+    v = vec([1, 2, 3])
     assert len(v) == 3
 
 
-"""
+def test_conj():
+    # Like list.append, except returns new vector w/ structural sharing.
+    v0 = vec()
+    assert len(v0) == 0
+    v1 = v0.cons(1)
+    assert len(v0) == 0
+    assert len(v1) == 1
+    assert v1[0] == 1
+
+
 def test_indexing_pos():
-    v = Vector([1, 2, 3])
+    v = vec([1, 2, 3])
     assert v[0] == 1
     assert v[1] == 2
     assert v[2] == 3
 
+"""
+
+def test_big_vector():
+    # TODO: test a vector that pushes the limits of len(), etc.
+    # See if we can get it to break when using a list over the maxint size.
+    pass
+
 
 def test_indexing_neg():
-    v = Vector([1, 2, 3])
+    v = vec([1, 2, 3])
     assert v[-3] == 1
     assert v[-2] == 2
     assert v[-1] == 3
@@ -31,23 +47,13 @@ def test_indexing_neg():
 def test_equality_with_list_and_tuple():
     l = range(10, 20)
     t = tuple(l)
-    v = Vector(l)
+    v = vec(l)
     assert l == v
     assert t == v
 
 
-def test_conj():
-    # Like list.append, except returns new vector w/ structural sharing.
-    v0 = Vector()
-    assert len(v0) == 0
-    v1 = v0.conj(1)
-    assert len(v0) == 0
-    assert len(v1) == 1
-    assert v1[0] == 1
-
-
 def test_assoc():
-    v0 = Vector([1, 2, 3])
+    v0 = vec([1, 2, 3])
     v1 = v0.assoc(0, 10)
     assert v0 == [1, 2, 3]
     assert v1 == [10, 2, 3]
@@ -59,7 +65,7 @@ def test_assoc():
 def test_pop():
     # TODO XXX: "pop" in clojure returns a new vec with the last element
     # removed.  Consider renaming to not collide with Python's list.pop.
-    v0 = Vector([1, 2])
+    v0 = vec([1, 2])
     v1 = v0.pop()
     assert v0 == [1, 2]
     assert v1 == [1]
@@ -75,47 +81,47 @@ def test_replace():
 
 
 def test_hash():
-    v0 = Vector()
+    v0 = vec()
     assert isinstance(hash(v0), int)
-    v1 = Vector()
+    v1 = vec()
     assert hash(v0) == hash(v1)
     vals = range(10)
     shuffle(vals)
-    v0 = Vector(vals)
-    v1 = Vector(vals)
+    v0 = vec(vals)
+    v1 = vec(vals)
     assert hash(v0) == hash(v1)
 
 
 def test_iteration():
     l = [10, 11, 12]
-    vec = Vector(l)
+    vv = vec(l)
     nl = []
-    for v in vec:
+    for v in vv:
         nl.append(v)
-    assert vec == nl == l
+    assert vv == nl == l
 
 
 def test_slice():
-    vec = Vector([1, 2, 3, 4])
+    vv = vec([1, 2, 3, 4])
 
-    s0 = vec[:]
+    s0 = vv[:]
     assert s0 == vec
     
-    s1 = vec[2:]
+    s1 = vv[2:]
     assert s1 == [3, 4]
 
-    s2 = vec[::2]
+    s2 = vv[::2]
     assert s2 == [1, 3]
 
-    s3 = vec[1::2]
+    s3 = vv[1::2]
     assert s3 == [2, 4]
 
    # TODO: test negative indices...
 
 
 def test_coercion_to_list_tuple():
-    vec = Vector(range(10))
-    ll = list(vec)
+    vv = vec(range(10))
+    ll = list(vv)
     assert isinstance(ll, list)
     assert ll == range(10)
     tt = tuple(vec)
--- vector.pyx
+++ vector.pyx
@@ -1,19 +1,38 @@
-
-DEF NN = 32
 DEF SHIFT = 5
+DEF NN = 2**SHIFT
 
 cdef class Node:
+
     cdef list _array
+
     def __cinit__(self, array=None):
         if array is None:
             self._array = [None] * NN
         else:
             self._array = array
 
+
+cdef int _tailoff(cnt):
+    # TODO: Check to make sure this works properly with C ints & C
+    # semantics.
+    # FIXME: Should self._cnt be unsigned, or py_ssize_t?  Use whatever is
+    # used for Python List sizes...
+    if cnt < NN:
+        return 0
+    # TODO: FIXME: 5 is a magic number here.  Should it be SHIFT instead?
+    return ((cnt - 1) >> SHIFT) << SHIFT
+
+
 cdef Node EMPTY_NODE = Node()
 
+def vec(*args):
+    if len(args) <= 1:
+        seq = args[0] if len(args) == 1 else []
+        return PersistentVector.from_sequence(seq)
+    if len(args) == 4:
+        return PersistentVector(*args)
 
-cdef class Vector:
+cdef class PersistentVector:
 
     cdef:
         int _cnt # TODO: NOTE: should this be a py_ssize_t or somesuch?
@@ -21,19 +40,19 @@ cdef class Vector:
         Node _root
         list _tail
 
-    def __init__(self, seq=None):
+    @classmethod
+    def from_sequence(cls, seq):
         seq = seq or []
         if not isinstance(seq, (list, tuple)):
             msg = "converting from types other than list or tuple not yet supported."
             raise NotImplementedError(msg)
         cnt = len(seq)
         if cnt < 32:
-            self._init(cnt, SHIFT, EMPTY_NODE, list(seq))
+            return cls(cnt, SHIFT, EMPTY_NODE, list(seq))
         else:
             raise NotImplementedError("more than %d elements not yet supported." % NN)
 
-
-    def _init(self, cnt, shift, Node root, list tail):
+    def __init__(self, cnt, shift, Node root, list tail):
         self._cnt = cnt
         self._shift = shift
         self._root = root
@@ -41,6 +60,34 @@ cdef class Vector:
 
     def __len__(self):
         return self._cnt
+
+    def __getitem__(self, i):
+        cdef list node = self._array_for(i)
+        return node[i & 0x01f]
+
+    cdef list _array_for(self, int i):
+        cdef Node node
+        cdef int level
+        if 0 <= i < self._cnt:
+            if i >= _tailoff(self._cnt):
+                return self._tail
+            node = self._root        
+            level = self._shift
+            while level > 0:
+                node = node._array[(i >> level) & 0x01f]
+                level -= SHIFT
+            return node._array
+        raise IndexError()
+
+
+    def cons(self, val):
+        cdef list newtail
+        if self._cnt - _tailoff(self._cnt) < NN:
+            newtail = self._tail + [val]
+            return PersistentVector(self._cnt + 1, self._shift, self._root, newtail)
+        raise NotImplementedError("more than %d elements not yet supported." % NN)
+
+
 
 cdef Node editable_root(Node root):
     return Node(root._array[:])
@@ -66,3 +113,37 @@ cdef class TransientVector:
     
     # def __init__(self, cnt, shift, Node root, list tail):
         # self._cnt = 
+
+
+    def ensure_editable(self):
+        # TODO: detect when editing after making it persistent...
+        pass
+
+
+    def conj(self, val):
+        self.ensure_editable()
+        if self._cnt - _tailoff(self._cnt) < NN:
+            self._tail[self._cnt & 0x01f] = val
+            self._cnt += 1
+            return self
+        cdef Node newroot
+        cdef Node tailnode = Node(self._tail)
+        self._tail = [None] * NN
+        self._tail[0] = val
+        cdef int newshift = self._shift
+        if (self._cnt >> 5) > (1 << self._shift):
+            newroot = Node()
+            newroot._array[0] = self._root
+            newroot.array[1] = self.new_path(self._shift, tailnode)
+            newshift += 5
+        else:
+            newroot = self.push_tail(self._shift, self._root, tailnode)
+        self._root = newroot
+        self._shift = newshift
+        self._cnt += 1
+        return self
+
+
+    def persistent(self):
+        self.ensure_editable()
+        assert False
