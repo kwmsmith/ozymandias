ff25dc66905eb64e4ba19cb02080860177d56995
Kurt Smith <kwmsmith@gmail.com>
2015-10-12 08:24:35 -0500

    Fix block quoting in README.

--- vector.pyx
+++ vector.pyx
@@ -34,7 +34,22 @@ def vec(*args):
     if len(args) == 4:
         return PersistentVector(*args)
 
-cdef class PersistentVector:
+cdef class APersistentVector:
+
+    cdef:
+        long _hash
+
+    def __str__(self):
+        # TODO: implement, with some intelligence for really big vectors...
+        return "TODO"
+
+    # TODO: should we implement __iter__ here instead on PersistentVector?
+
+    def __richcmp__(lhs, rhs, int op):
+        if op == 2: # equality (==) test.
+
+
+cdef class PersistentVector(APersistentVector):
 
     cdef:
         int _cnt # TODO: NOTE: should this be a py_ssize_t or somesuch?
